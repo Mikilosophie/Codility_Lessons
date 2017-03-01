84a135f03f410575123dbe08691db280f3061272
Mikilosophie <arthurzou@163.com>
2017-03-01 18:11:03 +1300

    L11T01_CountSemiprimes_v01.c Analysis summary
    15:50 start:
    Code, test, and re-design.
    
    18:00 run test, passed.
    18:05 submit:
    
    
    Correctness	Performance	Task score
    Correctness	Performance	Task score
    100% 0%	44%
    
    Performance tests
    ▶ medium_random 
    small random, length = ~300 ✘TIMEOUT ERROR 
    running time: 0.74 sec., time limit: 0.10 sec.
    ▶ large_small_slices 
    large with very small slices, length = ~30,000 ✘TIMEOUT ERROR 
    running time: >6.00 sec., time limit: 0.10 sec.
    ▶ large_random1 
    large random, length = ~30,000 ✘TIMEOUT ERROR 
    running time: >6.00 sec., time limit: 0.10 sec.
    ▶ large_random2 
    large random, length = ~30,000 ✘TIMEOUT ERROR 
    running time: >6.00 sec., time limit: 0.10 sec.
    ▶ extreme_large 
    all max ranges ✘TIMEOUT ERROR 
    running time: >6.00 sec., time limit: 0.10 sec.

--- L11T01_CountSemiprimes_v01.c
+++ L11T01_CountSemiprimes_v01.c
@@ -1,1 +1,82 @@
+#include <math.h>
 
+int Total;
+
+struct Results solution(int N, int P[], int Q[], int M) {
+    struct Results result;
+    // write your code in C99 (gcc 6.2.0)
+    result.A = (int *)malloc( 2 * N * sizeof(int) );
+    result.M = 0;
+    
+    Total = N;
+    
+    int primeCounter;
+    
+    int semiprime(int candidate);
+    
+    for(int K=0; K<M; K++)
+    {
+        primeCounter=0;
+            
+        for(int PK = P[K]; PK <= Q[K]; PK++)
+        {
+            //printf("(PK): %d\n", PK);
+            //printf("semiprime(PK): %d\n", semiprime(PK));
+            
+            if( semiprime(PK) == 1)
+            {
+                primeCounter++;
+                //printf("primeCounter: %d\n", primeCounter);
+            }           
+            
+        }
+        
+        *(result.A+result.M) = primeCounter;
+        
+        result.M++;    
+    
+    }
+    
+    //free(result.A);  // result should be initialted in other function, so result.A can be freed out there.   
+    return result;
+}
+
+int semiprime(int candidate)
+{	
+	int prime(int n);
+	
+	for(int a=2; a<=Total; a++)
+	{
+		
+		if( ( prime(a)==1 ) && ( ( candidate % a ) == 0 ) )
+		{
+		
+    		for(int b=2; b<=Total; b++)
+    		{
+    		    if( ( prime(b)==1 ) && (a * b == candidate) )    		    
+    		        return 1;
+    		    
+    		}
+		
+		}
+		
+		
+	} //end of a loop
+	return -1;
+}
+
+int prime(int n)
+{
+	for(int divisor=2; divisor<n; divisor++)
+	{
+		//printf("n: %d\n", n);
+		//printf("divisor: %d\n", divisor);
+		//printf("n/divisor: %d\n", n%divisor);
+		
+			
+		if(n % divisor == 0)
+			return -1;
+	}
+	
+	return 1;
+}
